afebe21c4ff55859edc4c32cf44ed489bb7dd3e1
jonasdevzero <jonasdevzero@gmail.com>
2024-03-09 09:46:49 -0300

    feat: improve load email template helper

--- src/data/helpers/loadEmailTemplate.ts
+++ src/data/helpers/loadEmailTemplate.ts
@@ -1,14 +1,17 @@
 import { renderAsync } from '@react-email/render';
 import { emails } from '@infra/mail/templates';
+import { ENV } from '@main/config/env';
 
-type ArgumentTypes<F extends Function> = F extends (props: infer A) => any
-	? A
-	: never;
+type ArgumentTypes<F extends Function> = F extends (props: infer A) => any ? A : never;
+
+const isDevelopment = ENV.NODE_ENV === 'development';
 
 export async function loadEmailTemplate<
 	K extends keyof typeof emails,
 	D = ArgumentTypes<(typeof emails)[K]>,
 >(template: K, data: D) {
+	if (isDevelopment) return JSON.stringify(data);
+
 	const html = await renderAsync(emails[template](data as any));
 	return html;
 }
